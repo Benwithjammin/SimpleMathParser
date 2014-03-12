d450f9df57dcc21e4ed44388291d2a51531a2795
Ben Porter <jammin1984@gmail.com>
2014-03-12 18:24:56 +0000

    Added input translator, not sure if needed yet

---
+++ src/MathParser/MathParser.Tests.Unit/InputTranslatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace MathParser.Tests.Unit
+{
+    [TestFixture]
+    public class InputTranslatorTests
+    {
+        [Test]
+        [TestCase("a", '+')]
+        [TestCase("A", '+')]
+        [TestCase("b", '-')]
+        [TestCase("B", '-')]
+        [TestCase("c", '*')]
+        [TestCase("C", '*')]
+        [TestCase("d", '/')]
+        [TestCase("D", '/')]
+        [TestCase("e", '(')]
+        [TestCase("E", '(')]
+        [TestCase("f", '(')]
+        [TestCase("F", ')')]
+        public void GivenAnOperatorCharacterThenThisShouldBecomeTheRelevantOperator(string input, char expected)
+        {
+            var results = InputTranslator.Tokenise(input);
+
+            Assert.That(results.First(), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ src/MathParser/MathParser/InputTranslator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MathParser
+{
+    /*Convert the input into human readable collection of tokens based on the rules provided - a = '+', b = '-', c = '*', d = '/', e = '(', f = ')'
+     * maybe an uneeded step but seems sensible!*/
+    public class InputTranslator
+    {
+        public static List<char> Tokenise(string input)
+        {
+            
+        }
+    }
+}
